894b96032e2c953de9665965a22923fc32171368
Mark Lee <mark.lee@capricasoftware.co.uk>
2015-03-01 10:18:22 +0000

    Use a new full-screen strategy class from vlcj.

--- src/main/java/uk/co/caprica/vlcjplayer/VlcjPlayerFullScreenStrategy.java
+++ src/main/java/uk/co/caprica/vlcjplayer/VlcjPlayerFullScreenStrategy.java
@@ -4,60 +4,23 @@ import static uk.co.caprica.vlcjplayer.Application.application;
 
 import java.awt.Window;
 
-import uk.co.caprica.vlcj.player.embedded.AdaptiveRuntimeFullScreenStrategy;
-import uk.co.caprica.vlcj.player.embedded.windows.Win32FullScreenStrategy;
-import uk.co.caprica.vlcj.player.embedded.x.XFullScreenStrategy;
-import uk.co.caprica.vlcj.runtime.RuntimeType;
+import uk.co.caprica.vlcj.player.embedded.DefaultAdaptiveRuntimeFullScreenStrategy;
 import uk.co.caprica.vlcjplayer.event.AfterExitFullScreenEvent;
 import uk.co.caprica.vlcjplayer.event.BeforeEnterFullScreenEvent;
 
-final class VlcjPlayerFullScreenStrategy extends AdaptiveRuntimeFullScreenStrategy {
+final class VlcjPlayerFullScreenStrategy extends DefaultAdaptiveRuntimeFullScreenStrategy {
 
     VlcjPlayerFullScreenStrategy(Window window) {
-        setStrategy(RuntimeType.NIX, new NixFullScreenStrategy(window));
-        setStrategy(RuntimeType.WINDOWS, new WindowsFullScreenStrategy(window));
-        // FIXME Mac?
+        super(window);
     }
 
-    private class NixFullScreenStrategy extends XFullScreenStrategy {
-
-        private NixFullScreenStrategy(Window window) {
-            super(window);
-        }
-
-        @Override
-        protected void onBeforeEnterFullScreenMode() {
-            beforeEnterFullScreen();
-        }
-
-        @Override
-        protected void onAfterExitFullScreenMode() {
-            afterExitFullScreen();
-        }
-    }
-
-    private class WindowsFullScreenStrategy extends Win32FullScreenStrategy {
-
-        private WindowsFullScreenStrategy(Window window) {
-            super(window);
-        }
-
-        @Override
-        protected void onBeforeEnterFullScreenMode() {
-            beforeEnterFullScreen();
-        }
-
-        @Override
-        protected void onAfterExitFullScreenMode() {
-            afterExitFullScreen();
-        }
-    }
-
-    private void beforeEnterFullScreen() {
+    @Override
+    protected void beforeEnterFullScreen() {
         application().post(BeforeEnterFullScreenEvent.INSTANCE);
     }
 
-    private void afterExitFullScreen() {
+    @Override
+    protected  void afterExitFullScreen() {
         application().post(AfterExitFullScreenEvent.INSTANCE);
     }
 }
